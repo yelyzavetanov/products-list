c376c289590a846d7fad8db21827099ccd661414
yelyzavetanov <yelyzavetanov@gmail.com>
2025-10-03 16:32:44 +0300

    refactor: rename Supabase client to manager and update imports

---
+++ src/pkg/integrations/supabase/migrations/20251003131129_init.sql
@@ -0,0 +1,14 @@
+-- create schemas
+create schema if not exists business;
+create schema if not exists content;
+
+-- create table order_options in business
+create table if not exists business.order_options (
+  id text primary key,
+  option text
+);
+
+-- Fill order_options table with data
+insert into business.order_options (id, option) values
+  ('1', 'Direct'),
+  ('2', 'Reverse');
